2e270083cf483090a313a0b71a2fcb2c7f117704
timmethy476 <timmethy476@gmail.com>
2026-01-31 13:45:42 +0000

    restarting testing enviorment

--- searchforhelp-testing/searchforhelp-testing/api/helplines/category/[slug]/index.js
+++
@@ -1,8 +0,0 @@
-import { helplines } from '../../../../../lib/data';
-
-export default function handler(req, res) {
-  const { slug } = req.query;
-  const filteredHelplines = helplines.filter((helpline) => helpline.categoryId === slug);
-
-  res.status(200).json(filteredHelplines);
-}
--- searchforhelp-testing/searchforhelp-testing/api/helplines/featured.js
+++
@@ -1,6 +0,0 @@
-import { helplines } from '../../../lib/data';
-
-export default function handler(req, res) {
-  const featuredHelplines = helplines.filter((helpline) => helpline.isFeatured);
-  res.status(200).json(featuredHelplines);
-}
